90d8800b67fe26c11625942ab00d4e8de17ca0bf
Dennis Beatty <dennis@dcbeatty.com>
2021-03-28 11:00:19 -0700

    Get the user's IP address

--- lib/locator_web/live/page_live.ex
+++ lib/locator_web/live/page_live.ex
@@ -3,37 +3,17 @@ defmodule LocatorWeb.PageLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, query: "", results: %{})}
+    {:ok, put_connect_info(socket)}
   end
 
-  @impl true
-  def handle_event("suggest", %{"q" => query}, socket) do
-    {:noreply, assign(socket, results: search(query), query: query)}
+  defp put_connect_info(socket = %{connected?: false}) do
+    assign(socket, ip_address: nil)
   end
 
-  @impl true
-  def handle_event("search", %{"q" => query}, socket) do
-    case search(query) do
-      %{^query => vsn} ->
-        {:noreply, redirect(socket, external: "https://hexdocs.pm/#{query}/#{vsn}")}
-
-      _ ->
-        {:noreply,
-         socket
-         |> put_flash(:error, "No dependencies found matching \"#{query}\"")
-         |> assign(results: %{}, query: query)}
-    end
-  end
-
-  defp search(query) do
-    if not LocatorWeb.Endpoint.config(:code_reloader) do
-      raise "action disabled when not in development"
-    end
-
-    for {app, desc, vsn} <- Application.started_applications(),
-        app = to_string(app),
-        String.starts_with?(app, query) and not List.starts_with?(desc, ~c"ERTS"),
-        into: %{},
-        do: {app, vsn}
+  defp put_connect_info(socket) do
+    connect_info = get_connect_info(socket)
+    ip_tuple = connect_info[:peer_data][:address]
+    ip_string = ip_tuple |> :inet.ntoa() |> to_string()
+    assign(socket, ip_address: ip_string)
   end
 end
